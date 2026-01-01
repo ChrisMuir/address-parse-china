d56ece24bc8df9ff6ae635498b5f1517d00d6cb0
Chris Muir <chrismuirrva@gmail.com>
2026-01-01 16:53:13 -0500

    fix bug on long input strings

--- substring/substring.go
+++ substring/substring.go
@@ -8,17 +8,21 @@ const MAX_SUBSTRING_LEN = 15
 // Example input: "这是一个测试"
 // Output: map[这是:0 这是一:0 这是一个:0 这是一个测:0 这是一个测试:0 是一:1 是一个:1 是一个测:1 是一个测试:1 一个:2 一个测:2 一个测试:2 个测:3 个测试:3 测试:4]
 func GetAllSubstrings(location string) map[string]int {
+	var currSubStr []rune
 	loc := []rune(location)
 	maxLen := getMaxSubStrLen(len(loc))
 	subStrMap := make(map[string]int)
-	for i := 0; i < maxLen; i++ {
-		var currSubStr []rune
-		for j := i; j < maxLen; j++ {
-			currSubStr = append(currSubStr, loc[j])
-			if len(currSubStr) < MIN_SUBSTRING_LEN {
+	for i := 0; i < len(loc); i++ {
+		currSubStr = make([]rune, maxLen)
+		for j, subStrCounter := i, 0; j < (maxLen + i); j, subStrCounter = j+1, subStrCounter+1 {
+			if j == len(loc) {
+				break
+			}
+			currSubStr[subStrCounter] = loc[j]
+			if subStrCounter+1 < MIN_SUBSTRING_LEN {
 				continue
 			}
-			subStrMap[string(currSubStr)] = i
+			subStrMap[string(currSubStr[:subStrCounter+1])] = i
 		}
 	}
 	return subStrMap
